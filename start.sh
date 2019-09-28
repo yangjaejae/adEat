ecf5fd68a1b3516eb75aadbb23b07b1e23f18e58
DESKTOP-HMV62QE\under <undertakin@naver.com>
2019-09-28 22:31:42 +0900

    change container name

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-DOCKER_CONTAINER_NAME="lotto"
-DOCKER_TRAVELRE_EOS_API_IMAGE_NAME="lotto/nodejs"
-DOCKER_TRAVELRE_EOS_API_IMAGE_TAG="v1.0.0"
+DOCKER_CONTAINER_NAME="asEat"
+DOCKER_ADEAT_IMAGE_NAME="adEat/nodejs"
+DOCKER_ADEAT_IMAGE_TAG="v1.0.0"
 COMPOSE_FILE=docker-compose.yaml
-echo "start LOTTO Server"
+echo "start asEat Server"
 
 
 function restartContainers() {
@@ -12,16 +12,16 @@ function restartContainers() {
 }
 
 function rebuildDocker() {
-    echo "Stop and Remove current LOTTO Server docker container..."
+    echo "Stop and Remove current asEat Server docker container..."
     docker rm -f $(docker ps -aqf name="${DOCKER_CONTAINER_NAME}")
 
-    if [[ "$(docker images -q ${DOCKER_TRAVELRE_EOS_API_IMAGE_NAME}:${DOCKER_TRAVELRE_EOS_API_IMAGE_TAG} 2> /dev/null)" != "" ]]; then
-        echo "Remove TRAVELRE_EOS_API Server docker IMAGE!!.."
-        docker rmi -f ${DOCKER_TRAVELRE_EOS_API_IMAGE_NAME}:${DOCKER_TRAVELRE_EOS_API_IMAGE_TAG}
+    if [[ "$(docker images -q ${DOCKER_ADEAT_IMAGE_NAME}:${DOCKER_ADEAT_IMAGE_TAG} 2> /dev/null)" != "" ]]; then
+        echo "Remove adEat Server docker IMAGE!!.."
+        docker rmi -f ${DOCKER_ADEAT_IMAGE_NAME}:${DOCKER_ADEAT_IMAGE_TAG}
     fi
 
-    echo "Build TRAVELRE_EOS_API Server image.."
-    docker build -t ${DOCKER_TRAVELRE_EOS_API_IMAGE_NAME}:${DOCKER_TRAVELRE_EOS_API_IMAGE_TAG} .
+    echo "Build adEat Server image.."
+    docker build -t ${DOCKER_ADEAT_IMAGE_NAME}:${DOCKER_ADEAT_IMAGE_TAG} .
     docker-compose -f ${COMPOSE_FILE} up -d 2>&1
 }
 
@@ -30,11 +30,11 @@ function askProceed () {
   read -p "Remove CA-Registry Server images and container to rebuild those items(Y) or just restart container(N) ? [Y/N] " ans
   case "$ans" in
     y|Y|"" )
-      echo "Remove and rebuild docker container of LOTTO Server"
+      echo "Remove and rebuild docker container of asEat Server"
       rebuildDocker
     ;;
     n|N )
-      echo "Restart docker container of LOTTO Server..."
+      echo "Restart docker container of asEat Server..."
       restartContainers
     ;;
     * )
